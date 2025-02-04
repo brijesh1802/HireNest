409aea91b4b615ccde0e67de4deedda72a0c4597
Chayanka <chayanka2318@gmail.com>
2025-02-04 16:58:02 +0530

    --

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require('cors');
+const express = require('express');
+const dbConnect = require('./db/db');
+const candidateRoutes = require('./routes/candidate.route');
+const employerRoutes = require('./routes/employer.route');
+dbConnect();
+
+const app = express();
+const port = process.env.PORT || 8181;
+
+app.use(cors(
+    {
+        origin: process.env.VERCEL_URL,
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        credentials: true,
+        allowedHeaders: ['Content-Type', 'Authorization'],
+    }
+));
+app.use(cors({ origin: '*' }));
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+app.use('/api/v1/candidates', candidateRoutes);
+app.use('/api/v1/employers', employerRoutes);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to HireNest API');
+}
+);
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
